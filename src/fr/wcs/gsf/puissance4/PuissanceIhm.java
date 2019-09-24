b02b5e79a34fa9bb78e501ab02c40dce368a632e
rencho13 <stephone13@gmail.com>
2019-09-24 15:39:37 +0200

    Version fonctionnel

--- src/fr/wcs/gsf/puissance4/PuissanceIhm.java
+++ src/fr/wcs/gsf/puissance4/PuissanceIhm.java
@@ -2,40 +2,43 @@ package fr.wcs.gsf.puissance4;
 
 public class PuissanceIhm {
 	
-	final static int VIDE = 0 ;
-    final static int JAUNE = 1 ;
-    final static int ROUGE = 2;
-	
-	
-    static void initialise (int[][] grille) {
-        
-        for ( int i = 0; i < grille.length ; ++i) {
-            for ( int j = 0 ; j < grille[0].length ; ++j){
-                grille[i][j] = VIDE;
-            }
-        }
-    }
-  
-    static void affiche (int[][] grille){
-        System.out.println();
-        for ( int[] ligne : grille) {
-            System.out.print('\u2502');
-            for ( int cellule : ligne) {
-                if ( cellule == VIDE) {
-                    System.out.print(' '); }
-                else if ( cellule == ROUGE) {
-                    System.out.print('O'); }
-                else  {
-                    System.out.print('X'); }
-                System.out.print('|');
-                }
-            System.out.println();
-            }
-        for ( int i = 1 ; i <=grille[0].length; ++i)
-        {
-           System.out.print("="+i);
-        }   System.out.println("=\n");
-        }
 
+	
+	final static int VOID = 0;
+	final static int YELLOW = 1;
+	final static int RED = 2;
+
+	static void init(int[][] grid) {
+
+		for (int i = 0; i < grid.length; ++i) {
+			for (int j = 0; j < grid[0].length; ++j) {
+				grid[i][j] = VOID;
+			}
+		}
+	}
+
+	static void display(int[][] grid) {
+		System.out.println();
+		for (int[] ligne : grid) {
+			System.out.print('|');
+			for (int cell : ligne) {
+				if (cell == VOID) {
+					System.out.print(' ');
+				} else if (cell == RED) {
+					System.out.print('O');
+				} else {
+					System.out.print('X');
+				}
+				System.out.print('|');
+			}
+			System.out.println();
+		}
+
+		for (int i = 1; i <= grid[0].length; ++i) {
+			System.out.print("=" + i);
+		}
+		System.out.println("=\n");
+	}
+	
 	
 }
