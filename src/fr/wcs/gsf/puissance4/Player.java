03241c019a383234f36800f5d5b63931fd17597b
rencho13 <stephone13@gmail.com>
2019-09-27 13:53:14 +0200

    import class player

--- src/fr/wcs/gsf/puissance4/Player.java
+++ src/fr/wcs/gsf/puissance4/Player.java
@@ -1,10 +1,32 @@
 package fr.wcs.gsf.puissance4;
 
 
+
 public class Player {
-
-
-    
-    
 	
+	
+	public String name;
+	public String jeton;
+	
+	public Player(String name, String jeton) {
+		this.name = name;
+		this.jeton = jeton;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getJeton() {
+		return jeton;
+	}
+	public void setJeton(String jeton) {
+		this.jeton = jeton;
+	}
+	
+    public static void yourTurn(String arg) {
+    	System.out.println(arg);
+    }
 }
